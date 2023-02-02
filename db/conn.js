02cbed556394d0011ceae3a40f363f53a8d7d346
vladimirmoteiro <vladimir.monteiro021@gmail.com>
2023-02-02 13:35:19 -0300

    commit

--- db/conn.js
+++ db/conn.js
@@ -3,7 +3,7 @@ const moongose = require('mongoose')
 moongose.set('strictQuery', true)
 
 async function main () {
-    await moongose.connect(`mongodb+srv://${process.env.DB_USER_DEV}:${process.env.DB_PASSWORD_DEV}@up.wzrjx02.mongodb.net/?retryWrites=true&w=majority`)
+    await moongose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@db-mongodb-nyc1-53693-a8a5c363.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=db-mongodb-nyc1-53693`)
 
     console.log('conctado com sucesso ao MongoDB')
 }
