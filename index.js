cfd981a6d0da2b32eb39640e7802ffad31f724ff
vladimirmoteiro <vladimir.monteiro021@gmail.com>
2023-02-02 16:54:21 -0300

    tt

--- index.js
+++ index.js
@@ -48,14 +48,10 @@ app.get('/products', async (req,res)=> {
 
 })
 
-
-
-
-conn.then(() =>{
     app.listen(port , () => {
         console.log(`servidor rodando na porta ${port}`)
      })
 
-})
+
 
  
