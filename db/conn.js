4a595a2b61b1a48271ace4162e641358498138a3
vladimirmoteiro <vladimir.monteiro021@gmail.com>
2023-02-02 15:53:30 -0300

    com

--- db/conn.js
+++ db/conn.js
@@ -4,7 +4,7 @@ const moongose = require('mongoose')
 moongose.set('strictQuery', true)
 
 async function main () {
-    await moongose.connect(`mongodb+srv://vladimir:oM0gPYV2zeag9jZO@up.wzrjx02.mongodb.net/?retryWrites=true&w=majority`)
+    await moongose.connect(`mongodb+srv://doadmin:6c59O4rT8P0k2lS3@db-mongodb-nyc1-53693-a8a5c363.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=db-mongodb-nyc1-53693`)
 
     console.log('conctado com sucesso ao MongoDB')
 }
