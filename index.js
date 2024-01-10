6b5f587b304a68088e4b2e8d9f7097d368b48bad
vladimirmonteiro <vladimir.monteiro021@gmail.com>
2024-01-10 11:48:35 -0300

    config

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require ('express')
 const cors = require ('cors')
 const path = require('path')
 const app =  express()
+const helmet = require('helmet')
 
 
 const port = process.env.PORT || 8000
@@ -22,6 +23,13 @@ app.use(cors({credentials:true, origin:'*'}))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(helmet())
+
+app.use((req, res, next) => {
+    // Adiciona o cabeçalho X-Content-Type-Options à resposta
+    res.setHeader('X-Content-Type-Options', 'nosniff');
+    next();
+  });
 
 
 
